cc70076f76f4b0db33ba01b1d0b6d7253ffb7a78
Enzo Pontet <enzopch2022@gmail.com>
2025-05-01 11:55:28 -0300

    Sumando mas cositas

--- app/loading.tsx
+++ app/loading.tsx
@@ -9,7 +9,7 @@ const LoadingPage = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        width: "100vh",
+        width: "100vw",
       }}
     >
       <Image src={loader} height={150} width={150} alt="Loading..." />
--- db/seed.ts
+++ db/seed.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
 
+async function main() {
+  const prisma = new PrismaClient();
+  await prisma.product.deleteMany();
 
-async function main(){
-const prisma = new PrismaClient();
-await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
+  await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
-
-await prisma.product.createMany({data: sampleData.products});
-
-
-console.log('Database seeded sucsesfulyyyy');
-
-
+  console.log("Database seeded sucsesfulyyyy");
 }
 
-main()
+main();
