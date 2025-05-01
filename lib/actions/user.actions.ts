181168bae55512b2195b413904b2897693b3c12a
Enzo Pontet <enzopch2022@gmail.com>
2025-05-01 12:43:49 -0300

    Autenticacion Agregada

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,21 +3,22 @@
 import {
   shippingAddressSchema,
   signInFormSchema,
-  //   signUpFormSchema,
+  signUpFormSchema,
   //   paymentMethodSchema,
   updateUserSchema,
 } from "../validators";
 // import { auth, signIn, signOut } from "@/auth";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
-// import { hash } from '../encrypt';
+
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
 import { z } from "zod";
 // import { PAGE_SIZE } from '../constants';
 import { revalidatePath } from "next/cache";
-// import { Prisma } from "@prisma/client";
+import { hashSync } from "bcrypt-ts-edge";
+// import { prisma } from "@prisma/client";
 // import { getMyCart } from './cart.actions';
 
 // Sign in the user with credentials
@@ -56,40 +57,40 @@ export async function signOutUser() {
 }
 
 // Sign up user
-// export async function signUpUser(prevState: unknown, formData: FormData) {
-//   try {
-//     const user = signUpFormSchema.parse({
-//       name: formData.get("name"),
-//       email: formData.get("email"),
-//       password: formData.get("password"),
-//       confirmPassword: formData.get("confirmPassword"),
-//     });
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
 
-//     const plainPassword = user.password;
+    const plainPassword = user.password;
 
-//     user.password = await hash(user.password);
+    user.password = hashSync(user.password);
 
-//     await prisma.user.create({
-//       data: {
-//         name: user.name,
-//         email: user.email,
-//         password: user.password,
-//       },
-//     });
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
 
-//     await signIn("credentials", {
-//       email: user.email,
-//       password: plainPassword,
-//     });
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
 
-//     return { success: true, message: "User registered successfully" };
-//   } catch (error) {
-//     if (isRedirectError(error)) {
-//       throw error;
-//     }
-//     return { success: false, message: formatError(error) };
-//   }
-// }
+    return { success: true, message: "User registered successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: formatError(error) };
+  }
+}
 
 // Get user by the ID
 export async function getUserById(userId: string) {
